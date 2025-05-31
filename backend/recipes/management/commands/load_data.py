0d679c14029f0abeb19ef02e3a316317d0c88981
DmitryProtasevich <protas.dmitry4@yandex.ru>
2025-05-31 16:36:16 +0300

    edit load data add tags

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -4,31 +4,45 @@ import os
 from django.core.management import BaseCommand
 from django.conf import settings
 
-from recipes.models import Ingredient
+from recipes.models import Ingredient, Tag
+
+CSV_MODEL_MAP = {
+    'tags': Tag,
+    'ingredients': Ingredient,
+}
 
 
 class Command(BaseCommand):
-    """Класс загрузки тестовой базы данных ингредиентов."""
+    """Класс загрузки тестовой базы данных."""
 
     def handle(self, *args, **options):
-        path = os.path.join(settings.CSV_FILES_DIR, 'ingredients.csv')
-        if not os.path.exists(path):
-            self.stdout.write(self.style.WARNING(
-                'Файл ingredients.csv не найден.'
-            ))
-            return
-
-        self.stdout.write('Началась загрузка файла: ingredients.csv')
-        objects = []
-        with open(path, encoding='utf-8') as f:
-            for row in csv.DictReader(
-                f, fieldnames=('name', 'measurement_unit')
-            ):
-                objects.append(Ingredient(
-                    name=row['name'],
-                    measurement_unit=row['measurement_unit']
+        for file_name, model in CSV_MODEL_MAP.items():
+            path = os.path.join(settings.CSV_FILES_DIR, f'{file_name}.csv')
+            if not os.path.exists(path):
+                self.stdout.write(self.style.WARNING(
+                    f'Файл {file_name}.csv не найден.'
                 ))
-        Ingredient.objects.bulk_create(objects, ignore_conflicts=True)
-        self.stdout.write(self.style.SUCCESS(
-            'Загрузка ingredients.csv завершена'
-        ))
+                return
+            self.stdout.write(f'Началась загрузка файла: {file_name}.csv')
+            with open(path, encoding='utf-8') as f:
+                objects = []
+                if file_name == 'ingredients':
+                    reader = csv.DictReader(
+                        f, fieldnames=['name', 'measurement_unit']
+                    )
+                else:
+                    reader = csv.DictReader(f)
+                for row in reader:
+                    data = {}
+                    for field, value in row.items():
+                        if field.endswith('_id') and field != 'id':
+                            rel = model._meta.get_field(field[:-3]
+                                                        ).related_model
+                            data[field[:-3]] = rel.objects.get(pk=value)
+                        else:
+                            data[field] = value
+                    objects.append(model(**data))
+                model.objects.bulk_create(objects, ignore_conflicts=True)
+            self.stdout.write(self.style.SUCCESS(
+                f'Загрузка {file_name}.csv завершена'
+            ))
